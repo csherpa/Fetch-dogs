2110f4fa3a21f48e3e7b4865793d79e2fc5ffa49
Chhoti Sherpa <chhoti@wherewego.org>
2023-07-09 18:08:10 -0500

    /dogs/search route created and able to return correct value

--- src/server/api/routers/dogsRoute.ts
+++ src/server/api/routers/dogsRoute.ts
@@ -1,20 +1,60 @@
 import axios from "axios";
+import { z } from "zod";
 import {
   createTRPCRouter,
   publicProcedure,
   isAuthenticated,
 } from "~/server/api/trpc";
 
+const basePath = "https://frontend-take-home-service.fetch.com";
+
 export const dogsRouter = createTRPCRouter({
+  dogInfoObj: publicProcedure
+    .use(isAuthenticated)
+    .input(z.object({ id: z.string().array() }))
+    .query(async ({ input, ctx }) => {
+      const res = await axios({
+        method: "post",
+        url: `${basePath}/dogs`,
+        data: {
+          id: input.id,
+        },
+        headers: {
+          Cookie: `fetch-access-token=${ctx.cookie}`,
+        },
+      });
+      // console.log(res.data);
+      return { data: res.data };
+    }),
   breeds: publicProcedure.use(isAuthenticated).query(async ({ ctx }) => {
     const res = await axios({
       method: "get",
-      url: "https://frontend-take-home-service.fetch.com/dogs/breeds",
+      url: `${basePath}/dogs/breeds`,
       headers: {
         Cookie: `fetch-access-token=${ctx.cookie}`,
       },
     });
-
     return { data: res.data };
   }),
+  searchDogs: publicProcedure
+    .use(isAuthenticated)
+    .input(
+      z.object({
+        breeds: z.string().array(),
+      })
+    )
+    .query(async ({ input, ctx }) => {
+      const res = await axios({
+        method: "get",
+        url: `${basePath}/dogs/search`,
+        params: {
+          breeds: input.breeds,
+        },
+        headers: {
+          Cookie: `fetch-access-token=${ctx.cookie}`,
+        },
+      });
+      // console.log(res.data);
+      return { data: res.data };
+    }),
 });
