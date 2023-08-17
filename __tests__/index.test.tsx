fdea0668c161a9e9f9c0e06ba25d2807f3911d72
Chhoti Sherpa <chhoti@wherewego.org>
2023-08-17 14:32:06 -0500

    Create a simple mockData of the dog object in the test file

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,20 +1,38 @@
-import axios, { AxiosResponse } from "axios";
-import { test, expect } from "@jest/globals";
+import { test, expect, jest } from "@jest/globals";
+import { api } from "~/utils/api";
+import { render } from "@testing-library/react";
 
-// Mock the axios requests for testing
-jest.mock("axios");
-const mockedAxios = axios as jest.Mocked<typeof axios>;
-
-describe("Authentication", () => {
-  test("check if the user has successfully logged in", () => {
-    const mockResponse = {
-      data: {},
-      status: 200,
-      statusText: "ok",
-    } as AxiosResponse;
-    mockedAxios.get.mockResolvedValue(mockResponse);
-
-    // expect(mockedAxios).toHaveBeenCalledTimes(1);
-    // expect(response).toEqual({ data: {}, status: 200, statusText: "ok" });
-  });
+const mockQueryData = {
+  data: {
+    dogObj: [
+      {
+        id: 1,
+        name: "Emory",
+        breed: "Pug",
+        age: 5,
+        zip_code: 75010,
+        img: "https://frontend-take-home.fetch.com/dog-images/n02110958-pug/n02110958_8979.jpg",
+      },
+      {
+        id: 2,
+        name: "Sparkles",
+        breed: "Afghan Hound",
+        age: 1,
+        zip_code: 75010,
+        img: "https://frontend-take-home.fetch.com/dog-images/n02088094-Afghan_hound/n02088094_10263.jpg",
+      },
+    ],
+  },
+};
+describe("Dog Search Result Cards", () => {
+  // let apiDataMock;
+  // beforeEach(() => {
+  //   apiDataMock = jest
+  //     .spyOn(api, "useQueries")
+  //     .mockResolvedValue(mockQueryData);
+  // });
+  // afterEach(() => {
+  //   jest.resetAllMocks();
+  // });
+  test("renders dogs object from the query", () => {});
 });
